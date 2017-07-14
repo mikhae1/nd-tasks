0921564eed7fcca4fd1f80674c496f97fa5b0802
mmekhanov <mikhail.mekhanov@eurochem.ru>
2017-07-14 13:40:57 +0300

    Refactor audit log generation

--- tasks/audit.js
+++ tasks/audit.js
@@ -11,22 +11,29 @@ var debug = require('debug')('audit');
 
 var config = {
   src: [
-    // '/Users/mmekhanov/repos/flow',
-    // '/Users/mmekhanov/repos/org-deals',
-    // '/Users/mmekhanov/repos/org-documents',
-    '/Users/mmekhanov/repos/org-models',
-    // '/Users/mmekhanov/repos/org-shipments',
-    // '/Users/mmekhanov/repos/org-transports',
-    // '/Users/mmekhanov/repos/org-counterparts',
-    // '/Users/mmekhanov/repos/org-yves',
+    resolveHome('~/repos/flow'),
+    resolveHome('~/repos/org-deals'),
+    resolveHome('~/repos/org-documents'),
+    resolveHome('~/repos/org-models'),
+    resolveHome('~/repos/org-shipments'),
+    resolveHome('~/repos/org-transports'),
+    resolveHome('~/repos/org-counterparts'),
+    resolveHome('~/repos/org-yves'),
   ],
 
-  dst: '/Users/mmekhanov/tmp/AUDIT.md',
+  dst: resolveHome('~/tmp/AUDIT.md'),
 
   filter: {
     from: moment('2017-01-01')
   }
 };
+
+function resolveHome(filepath) {
+  if (filepath[0] === '~')
+    return path.join(process.env.HOME, filepath.slice(1));
+
+  return filepath;
+}
 
 gulp.task('audit', function(gulpCallback) {
   var tasks = [];
@@ -49,14 +56,14 @@ gulp.task('audit', function(gulpCallback) {
 
     var content = '';
     for (var i = 0; i < data.length; i++) {
-      content += '\n#' + path.basename(config.src[i]) + '\n';
+      content += '\n# ' + path.basename(config.src[i]) + '\n\n';
       for (var j = 0; j < data[i].length; j++) {
         if (data[i][j].ts !== 'unknown') {
           content += data[i][j].text;
           content += '###### ' + data[i][j].ts;
         }
       }
-      content += '---';
+      content += '\n---';
     }
     debug(content);
 
@@ -120,33 +127,43 @@ gulp.task('audit', function(gulpCallback) {
       rd.on('close', function(line) {
         console.log('END');
         findDates(tags);
-        //return cb(null, blocks);
       });
 
       function findDates(tags) {
         var tasks = [];
         tags.forEach(function(tag) {
-          tasks.push(lib.ignoreError(lib.localFactory('git show v' + tag + ' | grep Date', {
-            cwd: path.dirname(file)
-          })));
+          tasks.push(lib.ignoreError(
+            lib.localFactory('git show v' + tag + ' | grep Date || true', {
+              cwd: path.dirname(file)
+            })));
         });
 
         async.series(tasks, function(err, res) {
           var out = [];
+          let keepNext = false;
           for (var i = 0; i < res.length; i++) {
             var tag = tags[i];
             var ts = 'unknown';
+            let re = /Date:[\s]+(.*)/;
+
             if (res[i][0] === null) ts = res[i][1].stdout;
 
             blocks[tag].ts = ts;
 
             let filtered = false;
-            process.exit(1);
+            let tsParsed;
+            if (config.filter && config.filter.from) {
+              tsParsed = null;
+              if (ts.match(re))
+                tsParsed = moment(ts.match(re)[1]);
 
-            // if (config.filter && config.filter.from) {
-            //   console.log(ts);
-            //   process.exit(1);
-            // }
+              debug('ts parsed:', tsParsed);
+              if (tsParsed && tsParsed.isSameOrBefore(config.filter.from))
+                if (!keepNext)
+                  keepNext = true;
+                else
+                  filtered = true;
+            }
 
             if (!filtered) out.push(blocks[tag]);
           }
